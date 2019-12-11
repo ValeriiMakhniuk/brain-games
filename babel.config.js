8901e68e8d3777590a465ef9384844a3212e469e
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2019-12-11 16:47:54 +0300

    change targets to node only

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,10 @@
 module.exports = {
     "presets": [
-        [
-            "@babel/preset-env",
+        ["@babel/preset-env",
             {
                 targets: {
                     node: 'current',
-                    firefox: '60',
-                    chrome: '67',
-                    safari: '11.1',
-                },
+                }
             }
         ]
     ]
