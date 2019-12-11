1860f48f78c57642c0bfcc94cf55a74bfa705d06
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2019-12-11 22:42:57 +0300

    remove brain-games.js

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 install:
 	npm install;
 start:
-	npx babel-node src/bin/brain-games.js
+	npx babel-node src/bin/brain-even.js
 lint:
 	npx eslint .
 publish:
--- src/bin/brain-games.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import askUser from '..';
-
-console.log('Welcome to the Brain Games!');
-const userName = askUser('Whats your name?');
-console.log(`Hello ${userName}`);
