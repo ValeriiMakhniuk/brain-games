1c94f6b43cfcd0cbe14624d02f9dde48af155cad
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2019-12-13 18:48:22 +0300

    add brain-progression game

---
+++ src/bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import fireGame from '..';
+import { progressionGame } from '../games';
+
+fireGame(progressionGame, 3);
--- src/games/index.js
+++ src/games/index.js
@@ -1,9 +1,11 @@
 import evenGame from './brain-even';
 import calcGame from './brain-calc';
 import gcdGame from './brain-gcd';
+import progressionGame from './brain-progression';
 
 export {
   evenGame,
   calcGame,
   gcdGame,
+  progressionGame,
 };
