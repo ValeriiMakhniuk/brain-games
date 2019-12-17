0f39958bb8e04bef67d495ee61237ad7ed8052b5
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2019-12-17 14:55:59 +0300

    remove useless functions

--- src/share/askUserName.js
+++
@@ -1,3 +0,0 @@
-import readlineSync from 'readline-sync';
-
-export default (question) => readlineSync.question(`${question} `);
--- src/share/index.js
+++ src/share/index.js
@@ -1,9 +1,3 @@
-import askUser from './askUser';
 import createRandomNum from './createRandomNum';
-import askUserName from './askUserName';
 
-export {
-  askUser,
-  createRandomNum,
-  askUserName,
-};
+export { createRandomNum };
